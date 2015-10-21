56318f4feed0d50e243e0ee93df314423eeb8749
Omid Hezaveh <a@gik.fi>
2015-10-21 17:37:00 +0300

    add hello world app

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import './main.css';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+main();
+
+function main() {
+  const app = document.createElement('div');
+
+  document.body.appendChild(app);
+
+  ReactDom.render(<App />, app);
+}
+
